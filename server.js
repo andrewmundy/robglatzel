722e1ab5c413a93bcb3eff2eb03b29ca4d74aad4
andrewmundy <andrewmundy@gmail.com>
2018-01-17 00:55:38 -0800

    okay okay

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback');
 
-var connect = require('connect');
 
-// var app = connect()
-//     .use(history())
-//     .listen(3000);
 
 app = express();
 // app.use(history());
