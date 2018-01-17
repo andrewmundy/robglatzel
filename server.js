55653d3056bc0908c26503ec47d2f57f9a65d4a7
andrewmundy <andrewmundy@gmail.com>
2018-01-17 00:41:20 -0800

    app connect

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
 
+var connect = require('connect');
 
+var app = connect()
+    .use(history())
+    .listen(3000);
 
-app = express();
+// app = express();
 // app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
